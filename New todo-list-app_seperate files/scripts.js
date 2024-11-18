b85a01320fa27d69a821cd9196ed0ee9550cc8e6
Khandy Satriya <satriya.khandy77@gmail.com>
2024-11-18 19:07:08 +0700

    Initial commit

---
+++ New todo-list-app_seperate files/scripts.js
@@ -0,0 +1,42 @@
+﻿const taskInput = document.getElementById('taskInput');
+const addTaskBtn = document.getElementById('addTaskBtn');
+const taskList = document.getElementById('taskList');
+
+// Add a new task
+addTaskBtn.addEventListener('click', function() {
+        const taskText = taskInput.ariaValueMax.trim();
+
+        if (taskText !== "") {
+            // Create new List item
+            const li = document.createElement('li');
+            li.textContent = taskText;
+
+            // Add "Mark as Completed" button
+            const completeBtn = document.createElement('button');
+            completeBtn.textContent = 'Complete';
+            completeBtn.classList.add('complete-btn');
+            li.appendChild(completeBtn);
+
+            // Add "Delete" button
+            const DeleteBtn = document.createElement('button');
+            deleteBtn.textContent = 'Delete';
+            deleteBtn.classList.add('delete-btn');
+            li.appendChild(deleteBtn); 
+
+            // Append new task to the List
+            taskList.appendChild(li)
+
+            // Clear input field
+            taskInput.value = "";
+
+            // Complete task when clicked
+            completeBtn.addEventListener('click', function() {
+                li.classList.toggle('completed');
+            });
+
+            // Delete task when clicked
+            deleteBtn.addEventListener('click', function() {
+                taskList.removeChild(li);
+            });
+        }
+    });
